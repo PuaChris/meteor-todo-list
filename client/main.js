a5c1146c70525f99cae6780885bed764d9f21d08
PuaChris <chris.pua@mail.utoronto.ca>
2019-11-28 18:25:20 -0500

    Added security authorization that only allows a valid user to insert a new task (instead of just hiding the option to input a new task from the UI and moved all of the database logic into tasks.js so that it can be called anywhere instead of just being used in the event handlers

--- client/main.js
+++ client/main.js
@@ -8,3 +8,8 @@ import App from '../imports/ui/App.js';
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
 });
+
+// Type in command 'meteor npm install --save react react-dom' to add React view packages
+// Type in command 'meteor remove blaze-html-templates' to remove Blaze .html templates
+// Type in command 'meteor add static-html' to enable static .html templates
+// Type in command 'meteor remove insecure' to remove this package that allows the client to directly edit the database
